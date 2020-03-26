46921665302c1c192d4b7bfda289ff99b14a1581
Magno Vianna <mvianna@live.com>
2020-03-26 00:02:08 -0300

    Aplicando rotas

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+
 const express = require('express');
 
 const app = express(); 
+
+/*
+>>>>>>>>>>>>>>>Métodos HTTP<<<<<<<<<<<<<<<
+GET:      Buscar um informação do Back-end 
+POST:     Criar uma informação no Back-end 
+PUT:    Alterar uma informação no Back-end 
+DELETE: Deletar uma informação no Back-end 
+>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
+*/
 
 app.get('/', (Request, Response) =>{
     return Response.send('Hello'); 
