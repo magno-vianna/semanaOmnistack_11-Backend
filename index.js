0478829431a0442cf8dfa6897a2ec09727aef34e
Magno Vianna <mvianna@live.com>
2020-03-27 22:45:48 -0300

    Instalando NodemonDDD

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express(); 
 
+app.use(express.json()); 
+
 /*
 >>>>>>>>>>>>>>>Métodos HTTP<<<<<<<<<<<<<<<
 GET:      Buscar um informação do Back-end 
