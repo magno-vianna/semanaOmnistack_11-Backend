770a6848b93bca70cfc4823588092cd26642a7fa
Magno Vianna <mvianna@live.com>
2020-03-28 13:56:33 -0300

    Criando as migrations

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,15 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './src/database/db.sqlite'
-    }
+    },
+  
+    migrations: {
+      directory: './src/database/migrations'
+
+    },
+
+    useNullAsDefault: true, 
+  
   },
 
   staging: {
